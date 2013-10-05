9e323e989022d4229279ec286fe9ccb45e51a34d
volodymyr stadnyk <balodza@gmail.com>
2013-10-05 11:25:22 +0300

    Lazy Init implemented for factory

---
+++ src/main/patterns/lazyinitialization/model/Car.java
@@ -0,0 +1,9 @@
+package patterns.lazyinitialization.model;
+
+/**
+ * Date: 10/5/13
+ * Time: 11:11 AM
+ */
+public interface Car {
+    void drive();
+}
---
+++ src/main/patterns/lazyinitialization/model/CarBrand.java
@@ -0,0 +1,9 @@
+package patterns.lazyinitialization.model;
+
+/**
+ * Date: 10/5/13
+ * Time: 11:13 AM
+ */
+public enum CarBrand {
+    AUDI, HONDA, VOLVO
+}
