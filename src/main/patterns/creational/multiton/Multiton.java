eae5d177303da78fa8034f7926a8355a2b8c065a
volodymyr stadnyk <balodza@gmail.com>
2013-10-05 14:45:37 +0300

    Added Multiton.

--- src/main/patterns/creational/multiton/Multiton.java
+++ src/main/patterns/creational/multiton/Multiton.java
@@ -1,8 +1,29 @@
 package patterns.creational.multiton;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * Simple multiton with Lazy Init.
  * Date: 10/5/13
  * Time: 2:40 PM
  */
 public class Multiton {
+    private static final Map<String, Multiton> MULTITONS = new HashMap<String, Multiton>();
+
+    private Multiton(){
+    }
+
+    public static Multiton getInstance(String key){
+        Multiton instance = MULTITONS.get(key);
+        if(instance == null){
+            synchronized (Multiton.class){
+                if(MULTITONS.get(key)== null){
+                    instance = new Multiton();
+                    MULTITONS.put(key, instance);
+                }
+            }
+        }
+        return instance;
+    }
 }
